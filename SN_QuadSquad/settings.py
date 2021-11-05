2f6e34bde14636bddc03c5c919ffa12a020ffbf8
BuddhiChathurika <buddhi.18@business.mrt.ac.lk>
2021-11-05 21:29:17 +0530

    new changes added

--- SN_QuadSquad/settings.py
+++ SN_QuadSquad/settings.py
@@ -27,12 +27,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 # Application definition
 
 INSTALLED_APPS = [
     'sticksocial',
     'landing1',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
