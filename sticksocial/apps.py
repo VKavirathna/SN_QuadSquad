01700a131eeb9d6b81d29196c370d73097a66c08
Nithya <nithyasonali1@gmail.com>
2021-11-07 22:27:42 +0530

    code edit

--- sticksocial/apps.py
+++ sticksocial/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class SticksocialConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class socialstickConfig(AppConfig):
     name = 'sticksocial'
