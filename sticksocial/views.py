c24a5838f4574d1b83f99175cd43f0456e74f42c
BuddhiChathurika <buddhi.18@business.mrt.ac.lk>
2021-11-09 18:02:48 +0530

    Created profile file

--- sticksocial/views.py
+++ sticksocial/views.py
@@ -106,4 +106,15 @@ class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
+class ProfileView(View):
+    def get(self, request, pk, *args, **kwargs):
+        profile=UserProfile.objects.get(pk=pk)
+        user=profile.user
+        posts=Post.objects.filter(author=user).order_by('-created_on')
 
+        context={
+            'user': user,
+            'profile': profile,
+            'posts': posts
+        }
+        return render(request, 'sticksocial/PROFILE.html', context)
