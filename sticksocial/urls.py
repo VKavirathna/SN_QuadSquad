2d8d529e8c0385ea5a826589602e8a0bf197b2e1
VKavirathna <vimaya.18@business.mrt.ac.lk>
2021-11-09 14:36:55 +0530

    Post edit file edited

--- sticksocial/urls.py
+++ sticksocial/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import PostListView,PostDetailView
+from .views import PostListView,PostDetailView, PostDeleteView, CommentDeleteView, PostEditView
+
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='POST_LIST'),
     path('post/<int:pk>', PostDetailView.as_view(),name='POST_DETAIL'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='POST_DELETE'),
+    path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='COMMENT_DELETE'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='POST_EDIT'),
 ]
