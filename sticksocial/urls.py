e48830b8623c54a3990e2d769406df41e3bf3332
kaushsew <92161238+kaushsew@users.noreply.github.com>
2021-11-09 23:27:52 +0530

    Update urls.py
    Added new changes

--- sticksocial/urls.py
+++ sticksocial/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='POST_EDIT'),
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='PROFILE_EDIT'),
+    path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
+    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
 ]
