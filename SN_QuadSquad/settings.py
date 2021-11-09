c2122925cee179daa6ea203eec6727a03bf3fb45
BuddhiChathurika <buddhi.18@business.mrt.ac.lk>
2021-11-09 17:12:34 +0530

    Created media folder

--- SN_QuadSquad/settings.py
+++ SN_QuadSquad/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'POST_LIST'
 ACCOUNT_EMAIL_REQUIRED = True
