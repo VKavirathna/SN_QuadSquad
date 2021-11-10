f6aaf129edb1ddb07daffeace744883f063469d8
VKavirathna <92043780+VKavirathna@users.noreply.github.com>
2021-11-10 12:45:45 +0530

    Urls to likes & dislikes added

--- sticksocial/urls.py
+++ sticksocial/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListView,PostDetailView, PostDeleteView, CommentDeleteView, PostEditView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
+from .views import PostListView,PostDetailView, PostDeleteView, CommentDeleteView, PostEditView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDisLike
 
 urlpatterns = [
     path('', PostListView.as_view(), name='POST_LIST'),
     path('post/<int:pk>', PostDetailView.as_view(),name='POST_DETAIL'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='POST_DELETE'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='COMMENT_DELETE'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', AddDisLike.as_view(), name='dislike'),
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='POST_EDIT'),
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>', ProfileEditView.as_view(), name='PROFILE_EDIT'),
